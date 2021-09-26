81924b84372829e45a9073594b394ea31c898900
mal1313 <ih.monsterz@icloud.com>
2021-09-26 15:55:31 +0900

    server内容変更

--- quiz/server.js
+++ quiz/server.js
@@ -1,9 +1,15 @@
 // server.js
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
+// var express = require('express');
+// var path = require('path');
+// var serveStatic = require('serve-static');
+// app = express();
+// app.use(serveStatic(__dirname + "/dist"));
+// var port = process.env.PORT || 5000;
+// app.listen(port);
+// console.log('server started '+ port);
+
+const express = require('express');
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(express.static(__dirname + "/dist/"));
 app.listen(port);
-console.log('server started '+ port);
